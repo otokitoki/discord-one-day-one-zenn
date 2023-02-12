2aea6b25e59015be7c38c55ff6214930aec45f6b
とき <53075562+otokitoki@users.noreply.github.com>
2023-02-12 14:22:58 +0000

    10分毎に新規記事があるかチェックして投稿できる

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 このブログのAPIを利用しているから急に使えなくなるかも
 - https://zenn.dev/hellorusk/articles/db83908e15b6f1
+
+以下サイトから、10分ごとにアクセスされるようにしてる
+- https://uptimerobot.com
--- bot/keep_alive.py
+++ bot/keep_alive.py
@@ -1,23 +1,31 @@
 from flask import Flask
 from threading import Thread
 from bot import get_diff_article_urls, client
+import asyncio
 
 app = Flask('')
 
 
 @app.route('/')
-async def home():
+def home():
   diff_article_urls = get_diff_article_urls()
   if not diff_article_urls:
     print("empty diff_article_urls")
+    send_message("新規の記事はないよ")
   for url in diff_article_urls:
     print(url)
-    await client.get_channel(1072507059322507327).send(url)
+    send_message(url)
   return "hello!"
 
 
 def run():
   app.run(host='0.0.0.0', port=8080)
+
+
+def send_message(msg):
+  coroutine = asyncio.run_coroutine_threadsafe(
+    client.get_channel(1072507059322507327).send(msg), client.loop)
+  coroutine.result()
 
 
 def keep_alive():
